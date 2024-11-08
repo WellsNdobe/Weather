88d39359727456f125b4fddb7e6909f9af57117a
Wells <danielndobe850@gmail.com>
2024-11-08 16:36:56 +0200

    Get User Permission For Location

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,35 @@ import { Home } from "./home";
 import { ImageBackground } from "react-native";
 import backgroundImg from "../assets/images/background.jpg";
 import { s } from "../styles/indexStyles";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function index() {
+  const [coordinates, setCoordinates] = useState();
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  async function getUserLocation() {
+    const status = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({
+        lat: "48.85",
+        lng: "2.35",
+      });
+    }
+  }
+  console.log(coordinates);
   return (
     <ImageBackground
       imageStyle={s.img}
