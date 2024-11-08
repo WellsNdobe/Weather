be098ccde1e1e4114fbea5e76c5c79daabf0e8ed
Wells <danielndobe850@gmail.com>
2024-11-08 17:05:40 +0200

    Fixed Coords Data Type

--- app/weather.tsx
+++ app/weather.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export class weatherAPI {
-  static async fetchWeatherByCoords(coords) {
+  static async fetchWeatherByCoords(coords: { lat: any; lng: any }) {
     return (
       await axios.get(
         `https://api.open-meteo.com/v1/forecast?latitude=${coords.lat}&longitude=${coords.lng}&daily=weathercode,temperature_2m_max,sunrise,sunset,windspeed_10m_max&timezone=auto&current_weather=true`
